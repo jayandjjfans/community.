cc208c5d935b03dddc13f6c98a890661af3c6622
jayandjjfans <649844512@qq.com>
2020-04-19 09:49:24 +0800

    p25

--- src/main/java/life/machu/community/controller/IndexController.java
+++ src/main/java/life/machu/community/controller/IndexController.java
@@ -1,7 +1,12 @@
 package life.machu.community.controller;
 
+import life.machu.community.dto.PaginationDTO;
+import life.machu.community.dto.QuestionDTO;
+import life.machu.community.mapper.QuestionMapper;
 import life.machu.community.mapper.UserMapper;
+import life.machu.community.model.Question;
 import life.machu.community.model.User;
+import life.machu.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +15,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private QuestionService questionService;
     @Autowired
     private UserMapper userMapper;
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request,Model model,
+                       @RequestParam(name = "page",defaultValue = "1")Integer page,
+                        @RequestParam(name = "size",defaultValue = "2")Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies!=null&&cookies.length!=0) {
             for (Cookie cookie : cookies) {
@@ -32,8 +43,9 @@ public class IndexController {
             }
         }
 
+        PaginationDTO pagination=questionService.list(page,size);
 
-
+        model.addAttribute("pagination",pagination);
         
         return "index"; }
 }
---
+++ src/main/java/life/machu/community/service/QuestionService.java
@@ -0,0 +1,49 @@
+package life.machu.community.service;
+
+import life.machu.community.dto.PaginationDTO;
+import life.machu.community.dto.QuestionDTO;
+import life.machu.community.mapper.QuestionMapper;
+import life.machu.community.mapper.UserMapper;
+import life.machu.community.model.Question;
+import life.machu.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if (page<1){
+            page=1;
+        }
+        if (page>paginationDTO.getTotalPage()){
+            page=paginationDTO.getTotalPage();
+        }
+
+        Integer offset=size*(page-1);
+        List<Question> questions = questionMapper.list(offset,size);
+        List<QuestionDTO> questionDTOList=new ArrayList<>();
+        for (Question question : questions) {
+       User user=userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
+    }
+
+}
