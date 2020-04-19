51e5d7122e028f0d8b0708169f0e29eb3c41cd4a
jayandjjfans <649844512@qq.com>
2020-04-19 18:31:55 +0800

    p29

--- src/main/java/life/machu/community/controller/IndexController.java
+++ src/main/java/life/machu/community/controller/IndexController.java
@@ -22,26 +22,12 @@ public class IndexController {
 
     @Autowired
     private QuestionService questionService;
-    @Autowired
-    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(HttpServletRequest request,Model model,
+    public String index(Model model,
                        @RequestParam(name = "page",defaultValue = "1")Integer page,
                         @RequestParam(name = "size",defaultValue = "2")Integer size){
-        Cookie[] cookies = request.getCookies();
-        if (cookies!=null&&cookies.length!=0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user=userMapper.findByToken(token);
-                    if (user!=null){
-                        request.getSession().setAttribute("user",user);
-                    }
-                    break;
-                }
 
-            }
-        }
 
         PaginationDTO pagination=questionService.list(page,size);
 
---
+++ src/main/java/life/machu/community/interceptor/WebConfig.java
@@ -0,0 +1,25 @@
+package life.machu.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+//@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(sessionInterceptor);
+                interceptorRegistration.addPathPatterns("/**");
+
+    }
+
+
+
+    }
+
